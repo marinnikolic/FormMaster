d0cd4f470a2a5d0517cfa2f0bb7f973f614db56f
mnikolic <marinnikolic2@gmail.com>
2018-11-29 09:42:05 +0100

    Adding method to get selectable picker object in FormElementPickerSingle

--- form-master/src/main/java/me/riddhimanadib/formmaster/model/FormElementPickerSingle.java
+++ form-master/src/main/java/me/riddhimanadib/formmaster/model/FormElementPickerSingle.java
@@ -12,9 +12,9 @@ public class FormElementPickerSingle extends BaseFormElement {
     private String pickerTitle; // custom title for picker
     private List<FormPickerSelectable> mOptions; // list of options for single and multi picker
     private List<String> mOptionsSelected; // list of selected options for single and multi picker
+    private int selectedPickerIndex;
 
-    public FormElementPickerSingle() {
-    }
+    public FormElementPickerSingle() { }
 
     public static FormElementPickerSingle createInstance() {
         FormElementPickerSingle formElementPickerSingle = new FormElementPickerSingle();
@@ -51,6 +51,14 @@ public class FormElementPickerSingle extends BaseFormElement {
     public FormElementPickerSingle setOptions(List<FormPickerSelectable> mOptions) {
         this.mOptions = mOptions;
         return this;
+    }
+
+    public void setSelectedPickerIndex(int i) {
+        this.selectedPickerIndex = i;
+    }
+
+    public FormPickerSelectable getSelectedObject() {
+        return mOptions.get(selectedPickerIndex);
     }
 
     public FormElementPickerSingle setOptionsSelected(List<String> mOptionsSelected) {
