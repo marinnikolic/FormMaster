442000fac059f68835fe4e0d3c2e3a4b674d8eec
mnikolic <marin.nikolic@sedamit.hr>
2018-12-14 13:38:18 +0100

    Changing setViewColor to setItemViewColor

--- form-master/src/main/java/me/riddhimanadib/formmaster/model/BaseFormElement.java
+++ form-master/src/main/java/me/riddhimanadib/formmaster/model/BaseFormElement.java
@@ -90,7 +90,7 @@ public class BaseFormElement {
         return this;
     }
 
-    public BaseFormElement setBacgroundColor(int color) {
+    public BaseFormElement setBackgroundColor(int color) {
         backgroundColor = color;
         return this;
     }
